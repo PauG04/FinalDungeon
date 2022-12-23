adc52c153de12cf7a408937b39653e6c3d4bb6ad
PauG04 <pau.giro@enti.cat>
2022-12-23 11:33:29 +0100

    final

--- EduDungeon/looting.cpp
+++ EduDungeon/looting.cpp
@@ -43,7 +43,7 @@ void WhatGear(int numberOfGear, Player& myPlayer) {
 		myPlayer.maxMoves += 1;
 		myPlayer.maxStamina += 30;
 		myPlayer.stamina += 30;
-		cout << "              > PAU's COCACOLA  +85 Gold  +1 Agility  +30 Stamina" << endl << "              > COCACOLA ESPUMA!!" << endl;
+		cout << "              > PAU\'s COCACOLA  +85 Gold  +1 Agility  +30 Stamina" << endl << "              > COCACOLA ESPUMA!!" << endl;
 
 		break;
 	case 2:
@@ -53,7 +53,7 @@ void WhatGear(int numberOfGear, Player& myPlayer) {
 		myPlayer.health -= 10;
 		myPlayer.maxStamina += 20;
 		myPlayer.stamina += 20;
-		cout << "              > AUGUSTUS' CROCS  +60 Gold  +1 Agility  -10 Health  +20 Stamina" << endl << "              > You feel comfortable" << endl;
+		cout << "              > AUGUSTUS\' CROCS  +60 Gold  +1 Agility  -10 Health  +20 Stamina" << endl << "              > You feel comfortable" << endl;
 
 		break;
 	case 3:
@@ -63,7 +63,7 @@ void WhatGear(int numberOfGear, Player& myPlayer) {
 		myPlayer.health -= 20;
 		myPlayer.maxStamina += 40;
 		myPlayer.stamina += 40;
-		cout << "              > AUGUSTUS’ CROCS (Sport mode)  +60 Gold  +2 Agility  -20 Health  +40 Stamina" << endl << "              > FIAUUUUUM!!" << endl;
+		cout << "              > AUGUSTUS\’ CROCS (Sport mode)  +60 Gold  +2 Agility  -20 Health  +40 Stamina" << endl << "              > FIAUUUUUM!!" << endl;
 
 		break;
 	case 4:
@@ -72,32 +72,32 @@ void WhatGear(int numberOfGear, Player& myPlayer) {
 		myPlayer.health -= 10;
 		myPlayer.maxStamina += 30;
 		myPlayer.stamina += 30;
-		cout << "              > VIETNAMITA's COOFFE  +85 Gold  -10 Health  +30 Stamina" << endl << "              > Que es De Chill y que le estas haciendo a mi abuela!?" << endl;
+		cout << "              > VIETNAMITA\'s COOFFE  +85 Gold  -10 Health  +30 Stamina" << endl << "              > Que es De Chill y que le estas haciendo a mi abuela!?" << endl;
 		break;
 	case 5:
 		myPlayer.gold += 4000;
 		myPlayer.maxHealth += 1;
 		myPlayer.health += 1;
-		cout << "              > MIVI's iPAD  +4000 Gold" << endl << "              > Think Different" << endl;
+		cout << "              > MIVI\'s iPAD  +4000 Gold" << endl << "              > Think Different" << endl;
 		break;
 	case 6:
 		myPlayer.gold += 350;
 		myPlayer.maxHealth += 30;
 		myPlayer.health += 30;
-		cout << "              > DANI's SWITCH  +350 Gold  +30 Health" << endl << "              > Smash that Kirby" << endl;
+		cout << "              > DANI\'s SWITCH  +350 Gold  +30 Health" << endl << "              > Smash that Kirby" << endl;
 		break;
 	case 7:
 		myPlayer.gold -= 20;
 		myPlayer.maxMoves += 1;
 		myPlayer.maxHealth -= 20;
 		myPlayer.health -= 20;
-		cout << "              > ALEX's SLAP  -20 Gold  +1 Agility  -20 Health" << endl << "              > AUCH!!" << endl;
+		cout << "              > ALEX\'s SLAP  -20 Gold  +1 Agility  -20 Health" << endl << "              > AUCH!!" << endl;
 		break;
 	case 8:
 		myPlayer.gold += 700;
 		myPlayer.maxStamina -= 20;
 		myPlayer.stamina -= 20;
-		cout << "              > CRAMI’s LOL ACCOUNT  +700 Gold  -20 Health" << endl << "              > Poor Guy..." << endl;
+		cout << "              > CRAMI\’s LOL ACCOUNT  +700 Gold  -20 Health" << endl << "              > Poor Guy..." << endl;
 		break;
 	case 9:
 		myPlayer.gold -= 200;
@@ -105,7 +105,7 @@ void WhatGear(int numberOfGear, Player& myPlayer) {
 		myPlayer.health -= 10;
 		myPlayer.maxStamina -= 10;
 		myPlayer.stamina -= 10;
-		cout << "              > PAU's HACKED LINK  -200 Gold  -10 Health  -10 Stamina" << endl << "              > Mmmm suspicious..." << endl;
+		cout << "              > PAU\'s HACKED LINK  -200 Gold  -10 Health  -10 Stamina" << endl << "              > Mmmm suspicious..." << endl;
 		break;
 	case 10:
 		myPlayer.gold += 25;
@@ -113,7 +113,7 @@ void WhatGear(int numberOfGear, Player& myPlayer) {
 		myPlayer.health += 50;
 		myPlayer.maxStamina -= 30;
 		myPlayer.stamina -= 30;
-		cout << "              > MARCOS' WAIFU SHIRT  +25 Gold  +50 Health  -30 Stamina" << endl << "              > To the Horny Jail" << endl;
+		cout << "              > MARCOS\' WAIFU SHIRT  +25 Gold  +50 Health  -30 Stamina" << endl << "              > To the Horny Jail" << endl;
 		break;
 
 	case 11:
@@ -123,7 +123,7 @@ void WhatGear(int numberOfGear, Player& myPlayer) {
 		myPlayer.maxStamina += 1;
 		myPlayer.stamina += 1;
 		myPlayer.maxMoves += 1;
-		cout << "              > MIVI's iPAD PRO  +4500 Gold  +1 Agility  +1 Health  +1 Stamina" << endl << "              > Think MUCH Different" << endl;
+		cout << "              > MIVI\'s iPAD PRO  +4500 Gold  +1 Agility  +1 Health  +1 Stamina" << endl << "              > Think MUCH Different" << endl;
 		break;
 
 	default:
